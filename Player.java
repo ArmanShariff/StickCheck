bf511e6ef2d01045bb1961bbe98377d30e4b2b8d
Kevin Hu <kevinhu273@gmail.com>
2020-07-12 16:35:41 -0400

    added all variables

--- Player.java
+++ Player.java
@@ -3,18 +3,26 @@
 public class Player extends Person{
     
     // fields
-    int stamina;
     int faceoff;
     int shooting;
+    int stamina;
+    int skating;
+    int strength;
+    int offensiveAwareness;
+    int defensiveAwareness;
     
     // constructor
     public Player(int stamina, int faceoff, int shooting, String pFirstName, String pLastName, String pPosition, String pCountry, int pAge, int pPlayerNumber, int pContract) {
 
         super(pFirstName, pLastName, pPosition, pCountry, pAge, pPlayerNumber, pContract);
 
-        this.stamina = stamina;
         this.faceoff = faceoff;
         this.shooting = shooting;
+        this.stamina = stamina;
+        this.skating = skating;
+        this.strength = strength;
+        this.offensiveAwareness = offensiveAwareness;
+        this.defensiveAwareness = defensiveAwareness;
 
     }
 }
