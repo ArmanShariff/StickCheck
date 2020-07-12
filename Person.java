44de3a41441e52f78ecec3a63f508b90ac727f4d
Kevin Hu <kevinhu273@gmail.com>
2020-07-12 15:58:51 -0400

    trying smth

---
+++ Person.java
@@ -0,0 +1,25 @@
+package Players;
+
+public class person {
+
+    String firstName;
+    String lastName;
+    String position;
+    String country;
+    int age;
+    int playerNumber;
+    int contract;
+    
+    public Player(String pFirstName, String pLastName, String pPosition, String pCountry, int pAge, int pPlayerNumber, int pContract) {
+        
+        firstName = pFirstName;
+        lastName = pLastName;
+        position = pPosition;
+        country = pCountry;
+        age = pAge;
+        playerNumber = pPlayerNumber;
+        contract = pContract;
+    
+    }
+
+}
