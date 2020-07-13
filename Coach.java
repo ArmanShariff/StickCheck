5f1597742dc4d39657f0a64eacea82a231db3385
Kevin Hu <kevinhu273@gmail.com>
2020-07-13 16:12:26 -0400

    created constructor

---
+++ Coach.java
@@ -0,0 +1,25 @@
+
+
+public class Coach extends Person {
+
+    //fields
+
+    String position;
+    String specialty:
+    int rating;
+
+    //constructor
+
+    public Coach(String position, String specialty, int rating, String pFirstName, String pLastName, String pPosition, String pCountry, int pAge, int pContract) {
+
+        super(pFirstName, pLastName, pPosition, pCountry, pAge, pContract);
+
+        this.position = position;
+        this.specialty = specialty;
+        this.rating = rating;
+
+    } 
+
+    //methods
+    
+}
