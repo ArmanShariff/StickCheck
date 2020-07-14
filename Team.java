628bceff25503a17d8d2c8e14aa21e4cac8aa3ad
Brian Widjaja <brianwidjaja20@gmail.com>
2020-07-14 12:48:27 -0400

    started startGame

--- Team.java
+++ Team.java
@@ -1,19 +1,51 @@
 public class Team {
+    private Player sRW;
+    private Player sC;
+    private Player sLW;
+    private Player sLD;
+    private Player sRD;
+    private Player bRW;
+    private Player bC;
+    private Player bLW;
+    private Player bLD;
+    private Player bRD;
+    private Goalie sG;
+    private Goalie bG;
 
-    public Player sRW;
-    public Player sLW;
-    public Player sC;
-    public Player sLD;
-    public Player sRD;
-    public Goalie sG;
-    public Player bRW;
-    public Player bLW;
-    public Player bC;
-    public Player bLD;
-    public Player bRD;
-    public Goalie bG;
+    private boolean userTeam;
 
-    public Team(Roster roster) {
+    public Team(boolean userTeamr) {
+        this.userTeam = userTeam;
     }
+
+    // public void addPlayer(Player player) {
+    //     playerRoster.add(player);
+    // }
+
+    // public ArrayList<Player> getPlayerRoster() {
+    //     return playerRoster;
+    // }
+
+    // public void addGoalie(Goalie goalie) {
+    //     goalieRoster.add(goalie);
+    // }
+
+    // public ArrayList<Goalie> getGoalieRoster() {
+    //     return goalieRoster;
+    // }
+
+    // public String toString() {
+    //     String printStatement = "Player Roster: \n";
+    //     for (int i = 0; i < playerRoster.size(); i++) {
+    //         printStatement += playerRoster.get(i) + ", ";
+    //     }
+
+    //     printStatement += "\nGoalie Roster: \n";
+    //     for (int j = 0; j < goalieRoster.size(); j++) {
+    //         printStatement += goalieRoster.get(j) + ", ";
+    //     }
+
+    //     return printStatement;
+    // }
 
 }
--- startGame.java
+++ startGame.java
@@ -1,3 +1,18 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class startGame {
-    
+    public static void main(String[] args) throws FileNotFoundException{
+        Roster roster = new Roster("roster.csv");
+
+        ArrayList<Player> playerList = new ArrayList<Player>();
+        ArrayList<Goalie> goalieList = new ArrayList<Goalie>();
+        ArrayList<Coach> coachList = new ArrayList<Coach>();
+        
+        playerList = roster.getPlayers();
+        goalieList = roster.getGoalies();
+        coachList = roster.getCoaches();
+
+        
+    }
 }
