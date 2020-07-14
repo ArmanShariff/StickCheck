816c0179aa82164a3b9ad6e31943710ca9c731d8
Brian Widjaja <brianwidjaja20@gmail.com>
2020-07-14 14:16:35 -0400

    July 14th- updated startGame

--- Team.java
+++ Team.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Team {
     private Player sRW;
     private Player sC;
@@ -11,41 +13,127 @@ public class Team {
     private Player bRD;
     private Goalie sG;
     private Goalie bG;
+    private Coach coach;
 
     private boolean userTeam;
 
-    public Team(boolean userTeamr) {
+    private ArrayList<Player> playerRoster = new ArrayList<Player>();
+    private ArrayList<Goalie> goalieRoster = new ArrayList<Goalie>();
+    private ArrayList<Coach> coachRoster = new ArrayList<Coach>();
+
+    public Team(boolean userTeam) {
         this.userTeam = userTeam;
     }
 
-    // public void addPlayer(Player player) {
-    //     playerRoster.add(player);
-    // }
+    public void addPlayer(Player player) {
+        playerRoster.add(player);
+    }
 
     // public ArrayList<Player> getPlayerRoster() {
-    //     return playerRoster;
+    // return playerRoster;
     // }
 
-    // public void addGoalie(Goalie goalie) {
-    //     goalieRoster.add(goalie);
-    // }
+    public void addGoalie(Goalie goalie) {
+        goalieRoster.add(goalie);
+    }
 
     // public ArrayList<Goalie> getGoalieRoster() {
-    //     return goalieRoster;
+    // return goalieRoster;
     // }
 
-    // public String toString() {
-    //     String printStatement = "Player Roster: \n";
-    //     for (int i = 0; i < playerRoster.size(); i++) {
-    //         printStatement += playerRoster.get(i) + ", ";
-    //     }
+    public void addCoach(Coach coach) {
+        coachRoster.add(coach);
+    }
 
-    //     printStatement += "\nGoalie Roster: \n";
-    //     for (int j = 0; j < goalieRoster.size(); j++) {
-    //         printStatement += goalieRoster.get(j) + ", ";
-    //     }
+    public void setPlayerPosition(Player player) {
+        if (player.getPosition().equals("Center")) {
+            if (sC == null) {
+                sC = player;
+            } else {
+                bC = player;
+            }
+        }
+        else if (player.getPosition().equals("Left Wing")) {
+            if (sLW == null) {
+                sLW = player;
+            } else {
+                bLW = player;
+            }
+        }
+        else if (player.getPosition().equals("Right Wing")) {
+            if (sRW == null) {
+                sRW = player;
+            } else {
+                bRW = player;
+            }
+        }
+        else if (player.getPosition().equals("Right Defence")) {
+            if (sRD == null) {
+                sRD = player;
+            } else {
+                bRD = player;
+            }
+        }
+        else if (player.getPosition().equals("Left Defence")) {
+            if (sLD == null) {
+                sLD = player;
+            } else {
+                bLD = player;
+            }
+        }
+    }
 
-    //     return printStatement;
-    // }
+    public void setGoaliePosition(Goalie goalie) {
+        if(goalie.getPosition().equals("Goaltender")) {
+            if (sG == null) {
+                sG = goalie;
+            } else {
+                bG = goalie;
+            }
+        }
+    }
+
+    public void setCoach(Coach coach) {
+        if(coach.getPosition().equals("Coach")) {
+            this.coach = coach;
+        }
+    }
+
+    public String toString() {
+        String printStatement = "Player Roster: \n";
+        printStatement += "Starting Center: " + sC + "\n";
+        printStatement += "Starting Left Wing: " + sLW + "\n";
+        printStatement += "Starting Right Wing: " + sRW + "\n";
+        printStatement += "Starting Left Defense: " + sLD + "\n";
+        printStatement += "Starting Right Defense: " + sRD + "\n";
+        printStatement += "Bench Center: " + bC + "\n";
+        printStatement += "Bench Left Wing: " + bLW + "\n";
+        printStatement += "Bench Right Wing: " + bRW + "\n";
+        printStatement += "Bench Left Defense: " + bLD + "\n";
+        printStatement += "Bench Right Defense: " + bRD + "\n";
+        printStatement += "\nGoalie Roster: \n";
+        printStatement += "Starting Goalie: " + sG + "\n";
+        printStatement += "Bench Goalie: " + bG + "\n";
+        printStatement += "\nCoach Roster: \n";
+        printStatement += "Coach: " + coach;
+
+
+        // String printStatement = "Player Roster: \n";
+        // for (int i = 0; i < playerRoster.size(); i++) {
+        //     printStatement += playerRoster.get(i) + "\n";
+        // }
+
+        // printStatement += "\nGoalie Roster: \n";
+        // for (int i = 0; i < goalieRoster.size(); i++) {
+        //     printStatement += goalieRoster.get(i) + "\n";
+        // }
+
+        // printStatement += "\nCoach Roster: \n";
+        // for (int i = 0; i < coachRoster.size(); i++) {
+        //     printStatement += coachRoster.get(i) + "\n";
+        // }
+
+        return printStatement;
+    }
 
 }
--- startGame.java
+++ startGame.java
@@ -2,17 +2,61 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class startGame {
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
+
+        
         Roster roster = new Roster("roster.csv");
+        Team toronto = new Team(true);
+        Team detroit = new Team(false);
+        Team chicago = new Team(false);
+        Team montreal = new Team(false);
+        Team newYork = new Team(false);
+        Team boston = new Team(false);
 
         ArrayList<Player> playerList = new ArrayList<Player>();
         ArrayList<Goalie> goalieList = new ArrayList<Goalie>();
         ArrayList<Coach> coachList = new ArrayList<Coach>();
-        
+
         playerList = roster.getPlayers();
         goalieList = roster.getGoalies();
         coachList = roster.getCoaches();
 
-        
+        sortTeam("Toronto", toronto, playerList, goalieList, coachList);
+        sortTeam("New York", newYork, playerList, goalieList, coachList);
+        sortTeam("Detroit", detroit, playerList, goalieList, coachList);
+        sortTeam("Chicago", chicago, playerList, goalieList, coachList);
+        sortTeam("Montreal", montreal, playerList, goalieList, coachList);
+        sortTeam("Boston", boston, playerList, goalieList, coachList);
+
+        System.out.println("Toronto: \n" + toronto);
+        System.out.println("\nNew York: \n" + newYork);
+        System.out.println("\nDetroit: \n" + detroit);
+        System.out.println("\nChicago: \n" + chicago);
+        System.out.println("\nMontreal: \n" + montreal);
+        System.out.println("\nBoston: \n" + boston);
     }
+
+    public static void sortTeam(String teamName,Team team, ArrayList<Player> playerRoster, ArrayList<Goalie> goalieRoster, ArrayList<Coach> coachRoster) {
+        for(int i = 0; i < playerRoster.size(); i++){
+            if(playerRoster.get(i).getCurrentTeam().equals(teamName)) {
+                team.addPlayer(playerRoster.get(i));
+                team.setPlayerPosition(playerRoster.get(i));
+            }
+        }
+
+        for(int i = 0; i < goalieRoster.size(); i++){
+            if(goalieRoster.get(i).getCurrentTeam().equals(teamName)) {
+                team.addGoalie(goalieRoster.get(i));
+                team.setGoaliePosition(goalieRoster.get(i));
+            }
+        }
+
+        for(int i = 0; i < coachRoster.size(); i++){
+            if(coachRoster.get(i).getCurrentTeam().equals(teamName)) {
+                team.addCoach(coachRoster.get(i));
+                team.setCoach(coachRoster.get(i));
+            }
+        }
+    }
+
 }
