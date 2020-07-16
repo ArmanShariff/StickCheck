688f082b8f6e14c78beafbc6c5d61cf56a506eb7
ArmanShariff <arman33shariff@gmail.com>
2020-07-16 12:35:30 -0400

    constructor in sim.java

--- Simulation.java
+++ Simulation.java
@@ -1,3 +1,27 @@
 class Simulation {
-    
+
+    // fields
+
+    Team teamA;
+    Team teamB;
+
+    // constructor
+
+    public Simulation(Team teamA, Team teamB) {
+
+        this.teamA = teamA;
+        this.teamB = teamB;
+        int period = 1200;
+
+    }
+
+    public static Boolean faceoffCalculation() {
+        
+    }
+
+
+
+
+
+
 }
