4f80af97ab62d873caed7abb33310c87aa717f5d
Kevin Hu <kevinhu273@gmail.com>
2020-07-31 17:06:57 -0400

    added frame

--- GUI.java
+++ GUI.java
@@ -3,8 +3,8 @@ class GUI{
     public static void main(String args[]){
        JFrame frame = new JFrame("My First GUI");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(300,300);
-       JButton button = new JButton("Press");
+       frame.setSize(1650,650);
+       JButton button = new JButton("STICK CHECK");
        frame.getContentPane().add(button); // Adds Button to content pane of frame
        frame.setVisible(true);
     }
