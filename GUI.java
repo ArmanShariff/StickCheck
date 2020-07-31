7b8146cc8f50e9122f39088974bc8fb489d94907
Kevin Hu <kevinhu273@gmail.com>
2020-07-31 17:29:04 -0400

    working on buttons

--- GUI.java
+++ GUI.java
@@ -1,11 +1,20 @@
+import java.awt.*;
 import javax.swing.*;
+
 class GUI{
     public static void main(String args[]){
-       JFrame frame = new JFrame("My First GUI");
+
+        //creating frame
+       JFrame frame = new JFrame("Stick Check");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(1650,650);
-       JButton button = new JButton("STICK CHECK");
-       frame.getContentPane().add(button); // Adds Button to content pane of frame
+       frame.setLayout(new FlowLayout());
+       
+       //creating button
+       JButton startButton = new JButton("Start Game");
+       startButton.setPreferredSize(new Dimension(300, 75));
+       startButton.setBounds(300, 300, 75, 175);
+       frame.getContentPane().add(startButton); // Adds Button to content pane of frame
        frame.setVisible(true);
     }
 }
