0c14a1835716764a25681faac2829840466486f0
ArmanShariff <arman33shariff@gmail.com>
2020-07-31 17:28:29 -0400

    add music

--- GUI.java
+++ GUI.java
@@ -1,11 +1,24 @@
+import java.nio.file.Paths;
+
 import javax.swing.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 class GUI{
     public static void main(String args[]){
+       music(); 
        JFrame frame = new JFrame("My First GUI");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(1650,650);
        JButton button = new JButton("STICK CHECK");
        frame.getContentPane().add(button); // Adds Button to content pane of frame
        frame.setVisible(true);
-    }
+       }
+
+       MediaPlayer mediaPlayer;
+       public void music() {
+           String s = "action.mp3";
+           Media h = new Media(Paths.get(s).toUri().toString());
+           mediaPlayer = new MediaPlayer(h);
+           mediaPlayer.play();
+    }   
 }
