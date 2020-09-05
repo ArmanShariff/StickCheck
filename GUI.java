ce9d005cf8fc9af2e39dbc7ae385547cedb12fab
Kevin Hu <kevinhu273@gmail.com>
2020-09-05 11:18:27 -0400

    did something I forget

--- GUI.java
+++ GUI.java
@@ -323,12 +323,8 @@ public class GUI {
         frame.add(startSimPanel);
     }
 
-<<<<<<< HEAD
     public void calendarScreen(Team team1, Team team2) {
-=======
-    public void calendarScreen() {
 
->>>>>>> 72297bc927952da9b916abbebc14c39af5a91f2a
         progress = "Calendar Screen";
         // disabling previous panels
         optionsPanel.setVisible(false);
